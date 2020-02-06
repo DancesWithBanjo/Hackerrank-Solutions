a233be921b4df250ed178af7c71836d739c19142
Christian <cawiemer922@gmail.com>
2020-02-05 19:20:44 -0800

    Adding first hackerrank solution

---
+++ twostring.py
@@ -0,0 +1,13 @@
+/*
+# Check to see if a substring exists between the two strings
+# Substring can be a single character
+# ---
+# Originally implimented using two "x in y" loops, but was too slow
+
+def twoStrings(s1, s2):
+
+    for i in range(len(s1)):
+        if s1[i] in s2:
+            return "YES"
+
+    return "NO"
